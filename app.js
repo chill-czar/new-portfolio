6c2fd37bbda42073e5eea09a1ddfad578fc017a2
Shivamvashist <ninjavashistsv1@gmail.com>
2024-08-27 19:12:26 +0530

    made a loader component and added js to remove it as site loads

---
+++ app.js
@@ -0,0 +1,17 @@
+// /$(window).on("load",function(){
+//     $(".loader-container").fadeOut("slow")
+// });
+
+var loader = document.getElementById("preloader");
+
+// function loaderr(){
+//     window.addEventListener("load",function(){
+//         loader.style.display = "none" ;
+//     })
+// };
+
+// setTimeout(loaderr(),2000);
+
+window.addEventListener("load",function(){
+    loader.style.display = "none" ;
+})
