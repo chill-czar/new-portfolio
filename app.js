0b251fac0c12833f8644cc79827d669b4208cd25
Shivamvashist <ninjavashistsv1@gmail.com>
2024-11-02 12:26:42 +0530

    Improved cursor and added a new slide

--- app.js
+++ app.js
@@ -4,20 +4,25 @@
 
 var loader = document.getElementById("preloader");
 
-// function loaderr(){
-//     window.addEventListener("load",function(){
-//         loader.style.display = "none" ;
-//     })
-// };
-
-// setTimeout(loaderr(),2000);
-
 window.addEventListener("load",function(){
-    loader.style.display = "none" ;
+    setTimeout(()=>{
+    loader.style.display = "none"
+    },2000) ;
 })
 
 
-//cursor
+// const cursorDot = document.querySelector(".cursor")
+
+
+
+// window.addEventListener("mousemove",(e)=>{
+//     const posX= e.clientX
+//     const posY= e.clientY
+//     cursorDot.style.left=`${posX}px`;
+//     cursorDot.style.top=`${posY}px`;
+// })
+
+
 
 const cursorDot = document.querySelector("[data-cursor]")
 
@@ -25,10 +30,20 @@ window.addEventListener("mousemove",function(e){
     const posX = e.clientX
     const posY = e.clientY
 
-    cursorDot.style.left=`${posX}px`;
-    cursorDot.style.top=`${posY}px`;
+    cursorDot.style.left=`${posX-15}px`;
+    cursorDot.style.top=`${posY-15}px`;
 })
 
-// window.addEventListener("click",function(){
-//     cursorDot.style.backgroundColor = red;
-// })
+
+window.addEventListener("mousedown", () => {
+    cursorDot.style.transform = "scale(0.5)";
+    // cursorDot.style.backgroundColor="rgba(255,0,0,0.7)"
+});
+
+// Reset the cursor after the click is released
+window.addEventListener("mouseup", () => {
+    cursorDot.style.transform = "scale(1)";
+    // cursorDot.style.backgroundColor="rgba(60,60,60,0.07)"
+});
+
+
