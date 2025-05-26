f1f54a259c517699e47d44fd81cbe791c263cfaa
Shivamvashist <ninjavashistsv1@gmail.com>
2025-05-26 21:16:02 +0530

    Fixed slow project tiles , cursor lag , updated cv

--- app.js
+++ app.js
@@ -40,10 +40,8 @@ window.addEventListener("mousemove",function(e){
     const posX = e.clientX
     const posY = e.clientY
 
-    this.setTimeout(()=>{
-        cursorDot.style.left=`${posX-15}px`;
-        cursorDot.style.top=`${posY-15}px`;
-    },100)
+    cursorDot.style.left=`${posX-15}px`;
+    cursorDot.style.top=`${posY-15}px`;
 
 
 })
@@ -70,62 +68,62 @@ function scrollToSection(id) {
     });
 }
 
-document.querySelectorAll(".particleCanvas").forEach((canvas) => {
-    const ctx = canvas.getContext("2d");
-    let particles = [];
+// document.querySelectorAll(".particleCanvas").forEach((canvas) => {
+//     const ctx = canvas.getContext("2d");
+//     let particles = [];
   
-    // Set canvas size based on element size
-    function resizeCanvas() {
-      canvas.width = canvas.offsetWidth;
-      canvas.height = canvas.offsetHeight;
-    }
+//     // Set canvas size based on element size
+//     function resizeCanvas() {
+//       canvas.width = canvas.offsetWidth;
+//       canvas.height = canvas.offsetHeight;
+//     }
   
-    resizeCanvas();
-    window.addEventListener("resize", resizeCanvas);
+//     resizeCanvas();
+//     window.addEventListener("resize", resizeCanvas);
   
-    class Particle {
-      constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.radius = Math.random() * 2 + 0.5;
-        this.speedX = Math.random() * 0.3 - 0.15;
-        this.speedY = Math.random() * 0.3 - 0.15;
-      }
+//     class Particle {
+//       constructor() {
+//         this.x = Math.random() * canvas.width;
+//         this.y = Math.random() * canvas.height;
+//         this.radius = Math.random() * 2 + 0.5;
+//         this.speedX = Math.random() * 0.3 - 0.15;
+//         this.speedY = Math.random() * 0.3 - 0.15;
+//       }
   
-      update() {
-        this.x += this.speedX;
-        this.y += this.speedY;
+//       update() {
+//         this.x += this.speedX;
+//         this.y += this.speedY;
   
-        if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
-        if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
-      }
+//         if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
+//         if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+//       }
   
-      draw() {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fillStyle = "#2d23c0";
-        ctx.shadowColor = "#2d23c0";
-        ctx.shadowBlur = 8;
-        ctx.fill();
-      }
-    }
+//       draw() {
+//         ctx.beginPath();
+//         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+//         ctx.fillStyle = "#2d23c0";
+//         ctx.shadowColor = "#2d23c0";
+//         ctx.shadowBlur = 8;
+//         ctx.fill();
+//       }
+//     }
   
-    function initParticles(count = 100) {
-      particles = [];
-      for (let i = 0; i < count; i++) {
-        particles.push(new Particle());
-      }
-    }
+//     function initParticles(count = 100) {
+//       particles = [];
+//       for (let i = 0; i < count; i++) {
+//         particles.push(new Particle());
+//       }
+//     }
   
-    function animate() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      particles.forEach((p) => {
-        p.update();
-        p.draw();
-      });
-      requestAnimationFrame(animate);
-    }
+//     function animate() {
+//       ctx.clearRect(0, 0, canvas.width, canvas.height);
+//       particles.forEach((p) => {
+//         p.update();
+//         p.draw();
+//       });
+//       requestAnimationFrame(animate);
+//     }
   
-    initParticles(50);
-    animate();
-  });
+//     initParticles(100);
+//     animate();
+//   });
